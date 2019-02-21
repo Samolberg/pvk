eb5510b808b1c5599d6c496dc2da85112eee6bae
Samolberg <l.samolberg@khk.ee>
2019-02-21 09:27:09 +0200

    korrektne väljatrükk

--- menu/index.php
+++ menu/index.php
@@ -10,12 +10,7 @@ $mainTmpl->set('title', 'Menu Application');
 
 $contentTmpl = new Template('content');
 
-$courseCardTmpl = new Template ('course_card');
 
-$courseCardHeaderTmpl = new Template('course_card_header');
-$courseCardDataTmpl = new Template ('course_card_data');
-
-$courseDatalistTmpl = new Template('course_data_list');
 
 $courses = array(
     array(
@@ -50,12 +45,26 @@ $courses = array(
     )
 );
 foreach ($courses as $course => $courseData) {
+    $courseCardTmpl = new Template ('course_card');
+    $courseCardHeaderTmpl = new Template('course_card_header');
+    $courseCardDataTmpl = new Template ('course_card_data');
+
     $courseCardHeaderTmpl->set('course_name', $courseData['name']);
     $courseCardHeaderTmpl->set('course_icon', $courseData['icon']);
     $courseCardTmpl->set('course_card_header', $courseCardHeaderTmpl->parse());
 
     $courseCardDataTmpl->set('course_name', $courseData['name']);
-    $courseCardDataTmpl->set('course_data_list', $courseDatalistTmpl->parse());
+    foreach ($courseData['data'] as $dish => $dishData){
+        $courseDatalistTmpl = new Template('course_data_list');
+
+        $courseDatalistTmpl->set('dish_name', $dishData['dish_name']);
+        $courseDatalistTmpl->set('dish_description', $dishData['dish_description']);
+        $courseDatalistTmpl->set('dish_price', $dishData['dish_price']);
+        $courseDatalistTmpl->set('discount', $dishData['discount']);
+
+        $courseCardDataTmpl->add('course_data_list', $courseDatalistTmpl->parse());
+    }
+
     $courseCardTmpl->set('course_card_data', $courseCardDataTmpl->parse());
 
 
